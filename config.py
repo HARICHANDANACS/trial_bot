e6de8467f1615c186c86dcdfdd37a21e01c51663
HARI CHANDANA C S <113349417+HARICHANDANACS@users.noreply.github.com>
2025-03-10 09:24:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Hugging Face API Configuration
-HF_API_KEY = "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"  # Replace with your API key
+HF_API_KEY = "key"  # Replace with your API key
 MODEL_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
